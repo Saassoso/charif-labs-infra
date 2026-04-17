aefa99df945bf61d2300795e19444934c18a9334
Saassoso <charifsaad38@gmail.com>
2026-04-17 15:47:14 +0100

    Update access.tf

--- terraform/access.tf
+++ terraform/access.tf
@@ -10,24 +10,20 @@ resource "cloudflare_zero_trust_access_identity_provider" "keycloak_oidc" {
     auth_url      = "https://auth.charif-labs.tech/realms/charif-labs/protocol/openid-connect/auth"
     token_url     = "https://auth.charif-labs.tech/realms/charif-labs/protocol/openid-connect/token"
     certs_url     = "https://auth.charif-labs.tech/realms/charif-labs/protocol/openid-connect/certs"
-    claims        = ["openid", "email", "profile", "groups"]
+    claims        = ["openid", "email", "profile", "groups", "ztna_role"]
   }
 }
 
 # --- Standalone Reusable Policy (v5 style) ---
-resource "cloudflare_zero_trust_access_policy" "wazuh_policy" {
+resource "cloudflare_zero_trust_access_policy" "admin_only_policy" {
   account_id = var.cloudflare_account_id
   name       = "Allow IT Admins Only"
   decision   = "allow"
 
   include = [{
-    email_domain = { domain = "charif-labs.tech" }
-  }]
-
-  require = [{
     oidc = {
       identity_provider_id = cloudflare_zero_trust_access_identity_provider.keycloak_oidc.id
-      claim_name           = "groups"
+      claim_name           = "ztna_role"
       claim_value          = "it-admin"
     }
   }]
@@ -46,7 +42,41 @@ resource "cloudflare_zero_trust_access_application" "wazuh_app" {
 
   # v5: policies are attached here, not on the policy resource
   policies = [{
-    id         = cloudflare_zero_trust_access_policy.wazuh_policy.id
+    id         = cloudflare_zero_trust_access_policy.admin_only_policy.id
     precedence = 1
   }]
 }
+
+# PORTAINER
+resource "cloudflare_zero_trust_access_application" "portainer_app" {
+  zone_id                   = var.cloudflare_zone_id
+  name                      = "Portainer Management"
+  domain                    = "mgmt.charif-labs.tech"
+  type                      = "self_hosted"
+  session_duration          = "8h"
+  allowed_idps              = [cloudflare_zero_trust_access_identity_provider.keycloak_oidc.id]
+  auto_redirect_to_identity = true
+  
+  # v5: policies are attached here, not on the policy resource
+  policies = [{
+    id         = cloudflare_zero_trust_access_policy.admin_only_policy.id
+    precedence = 1
+  }]
+}
+
+# AUTH Keycloak 
+#resource "cloudflare_zero_trust_access_application" "auth_admin_app" {
+#  zone_id                   = var.cloudflare_zone_id
+# name                      = "Keycloak Admin Console"
+# domain                    = "auth.charif-labs.tech/admin/*" # Protège uniquement l'accès admin
+# type                      = "self_hosted"
+# session_duration          = "8h"
+# allowed_idps              = [cloudflare_zero_trust_access_identity_provider.keycloak_oidc.id]
+# auto_redirect_to_identity = true
+# 
+# # v5: policies are attached here, not on the policy resource
+# policies = [{
+#   id         = cloudflare_zero_trust_access_policy.admin_only_policy.id
+#   precedence = 1
+# }]
+#}
