c36857ecb44f5aba027f061f69bcd0475d0d7b85
Saassoso <charifsaad38@gmail.com>
2026-05-20 16:59:37 +0100

    chore: updated tunnel ingress targets to reflect portainer container naming schemes

--- terraform/ingress.tf
+++ terraform/ingress.tf
@@ -17,19 +17,22 @@ resource "cloudflare_zero_trust_tunnel_cloudflared_config" "tunnel_config" {
         service  = "http://keycloak-server:8080"
       },
       {
+        # UPDATED
         hostname = "grafana.${var.domain_name}"
-        service  = "http://sovereign-stack-grafana-1:3000"
+        service  = "http://app-observability-grafana-1:3000"
       },
       {
+        # UPDATED
         hostname = "wazuh.${var.domain_name}"
-        service  = "https://sovereign-stack-wazuh.dashboard-1:5601"
+        service  = "https://app-security-wazuh.dashboard-1:5601"
         origin_request = {
           no_tls_verify = true
         }
       },
       {
+        # UPDATED
         hostname = "n8n.${var.domain_name}"
-        service  = "http://n8n:5678"
+        service  = "http://app-automation-n8n-1:5678"
       },
       {
         service = "http_status:404"
