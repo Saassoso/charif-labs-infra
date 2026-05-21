3f2cf372310eabf05f9fe126c6ec87b563ddf15b
Saassoso <charifsaad38@gmail.com>
2026-05-21 09:01:33 +0100

    Update access.tf

--- terraform/access.tf
+++ terraform/access.tf
@@ -107,3 +107,31 @@ resource "cloudflare_zero_trust_access_application" "auth_admin_app" {
     precedence = 1
   }]
 }
+
+# -----------------------------------------------------------
+# --- WEBHOOK BYPASS POLICY (FOR GITOPS/GITHUB CI/CD) ---
+# -----------------------------------------------------------
+
+
+resource "cloudflare_zero_trust_access_policy" "webhook_bypass_policy" {
+  account_id = var.cloudflare_account_id
+  name       = "Allow GitHub Webhooks Bypass"
+  decision   = "bypass"
+
+  include = [{
+    everyone = {}
+  }]
+}
+
+resource "cloudflare_zero_trust_access_application" "portainer_webhooks" {
+  zone_id          = var.cloudflare_zone_id
+  name             = "Portainer Webhooks Bypass"
+  # This targets the exact path without locking down the rest of Portainer
+  domain           = "mgmt.${var.domain_name}/api/stacks/webhooks"
+  type             = "self_hosted"
+
+  policies = [{
+    id         = cloudflare_zero_trust_access_policy.webhook_bypass_policy.id
+    precedence = 1
+  }]
+}
